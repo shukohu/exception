215b75c06355e87488a548f45fc915ea9b8d8443
Shukohu <dashievaviktoria06@gmail.com>
2024-09-27 16:48:46 +0800

    exception.iml

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            inputDataUser("SunnySunny", "Sungo6676", "Sungo6676");
+        } catch (WrongPasswordException | WrongLoginException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            test();
+        } catch (TestException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void test() {
+        throw new TestException();
+    }
+
+    public static void inputDataUser(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+        String regexLogin = "Sanny";
+        Pattern patternLogin = Pattern.compile(regexLogin);
+        Matcher matcherLogin = patternLogin.matcher(login);
+
+        String regexPassword = "[237727890w]&w";
+        Pattern patternPassword = Pattern.compile(regexPassword);
+        Matcher matcherPassword = patternPassword.matcher(password);
+
+
+        if (!matcherLogin.matches()) {
+            throw new WrongLoginException("Неверный логин");
+        }
+        if (!matcherPassword.matches()) {
+            throw new WrongPasswordException("Пароли не совпадают");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароли не совпадают");
         }
     }
 }
